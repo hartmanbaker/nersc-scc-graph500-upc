6e92c50946f4d633283b99de551781a07002bad4
ElizWang <eywang1999@gmail.com>
2016-06-11 23:49:38 -0700

    Main.c
    To run the code

--- Conversion.c
+++ Conversion.c
@@ -1,142 +1,32 @@
 /*
- * Conversion.c
+ * main.c
  *
- *  Created on: Jun 5, 2016
- *      Author: elizabeth wang
+ *  Created on: Jun 11, 2016
+ *      Author: elizabeth
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include "sglib.h"
-
-int path_counter = 0;
-
-int node_max = 0;
-
-struct Path {
-	int start;
-	int end;
-	int weight;
-};
-
-struct Path_List{
-	struct Path path;
-	struct Path_List *next_path_list_ptr;
-};
-
-/*
-int main(void){
-	printf("Start reading the input");
-}
-*/
-
-struct Path_List* readFile(){
-	struct Path_List *aList;
-	static struct Path_List *head;
-	head = NULL;
-	int r;
-	int start, end, weight = 0;
-	FILE* file;
-	file = fopen("Coords.txt", "r");
-	if (file == NULL){
-		printf("Failed to open file 'Coords.txt'");
-		exit(-1);
-	}else{
-		r = fscanf(file, "(%d %d %d)\n", &start, &end, &weight);
-		while(r != EOF){
-			path_counter++;
-			if(start > node_max){
-				node_max = start;
-			}
-			if(end > node_max){
-				node_max = end;
-			}
-			struct Path path;
-			path.start = start;
-			path.end = end;
-			path.weight = weight;
-
-			aList = malloc(sizeof(struct Path_List));
-			aList->path = path;
-
-			SGLIB_LIST_ADD(struct Path_List, head, aList, next_path_list_ptr);
-
-			printf("(%d %d %d)\n", start, end, weight);
-
-			r = fscanf(file, "(%d %d %d)\n", &start, &end, &weight);
-		}
-	}
-	fclose(file);
-
-	return head;
-}
-
-
-struct ilist {
-	int i;
-	struct ilist *next_ptr;
-};
-
-#define ILIST_COMPARATOR(e1, e2) (e1->i - e2->i)
+#include "conversion.h"
 
 int main(int argc, char **argv) {
 	struct Path_List* head = readFile();
+	int total_num_of_nodes = node_max+1;
 
-	// nodes range from 0 to node_max
-	int total_num_of_nodes = node_max + 1;
-	struct ilist* adjancency_list[total_num_of_nodes];
-	  int i,a;
-	for(i = 0; i < total_num_of_nodes; i++){
-		adjancency_list[i] = NULL;
-	}
+	struct ilist** adjacency_list = malloc(total_num_of_nodes * sizeof(struct ilist*));
+	int i;
 
-	struct ilist* li;
+	convert(head, adjacency_list, total_num_of_nodes);
 
-	struct Path_List* ptr = head;
-	while(ptr){
-		struct Path p = ptr->path;
-		printf("In main, (%d %d %d)\n", p.start, p.end, p.weight);
-
-		if(adjancency_list[p.start] != NULL){
-			struct ilist* al_head = adjancency_list[p.start];
-			li = malloc(sizeof(struct ilist));
-			li->i = p.end;
-			li->next_ptr = NULL;
-			SGLIB_LIST_ADD(struct ilist, al_head, li, next_ptr);
-			adjancency_list[p.start] = al_head;
-		}else{
-			li = malloc(sizeof(struct ilist));
-			li->i = p.end;
-			li->next_ptr = NULL;
-			adjancency_list[p.start] = li;
-		}
-
-		if(adjancency_list[p.end]){
-			struct ilist* al_head = adjancency_list[p.end];
-			li = malloc(sizeof(struct ilist));
-			li->i = p.start;
-			li->next_ptr = NULL;
-			SGLIB_LIST_ADD(struct ilist, al_head, li, next_ptr);
-			adjancency_list[p.end] = al_head;
-		}else{
-			li = malloc(sizeof(struct ilist));
-			li->i = p.start;
-			li->next_ptr = NULL;
-			adjancency_list[p.end] = li;
-		}
-
-		ptr = ptr->next_path_list_ptr;
-	}
 	printf("In total, there are %d paths.\n", path_counter);
 	printf("In total, there are %d nodes.\n", total_num_of_nodes);
 
-	for(i = 0; i < total_num_of_nodes; i ++){
+	for (i = 0; i < total_num_of_nodes; i++) {
 		printf("node %d: ", i);
-		if(adjancency_list[i] == NULL) {
+		if (adjacency_list[i] == NULL) {
 			printf("[]\n");
-		}else{
+		} else {
 			printf("[");
-			struct ilist* al_head = adjancency_list[i];
-			while(al_head != NULL){
+			struct ilist
+			*al_head = adjacency_list[i];
+			while (al_head != NULL) {
 				printf("%d => ", al_head->i);
 				al_head = al_head->next_ptr;
 			}
@@ -144,7 +34,6 @@ int main(int argc, char **argv) {
 		}
 	}
 
-  return(0);
+	return (0);
 }
-
 
